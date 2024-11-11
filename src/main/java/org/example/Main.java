1abcbdbfc6b9105564e743b231a4570d6fc1b35c
Zhongqing Chen <21210240135@m.fudan.edu.cn>
2024-11-11 14:34:57 +0800

    density SET

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,21 +12,22 @@ import java.util.*;
 
 public class Main {
 
-    public static String buildSimpleJoinQuery(int Col) {
+    public static String buildSimpleJoinQuery(int Col,int DENSITY) {
         StringBuilder defineBuilder = new StringBuilder();
         StringBuilder patternBuilder = new StringBuilder();
 
         // 构建 DEFINE 部分
         for (int i = 1; i <= Col; i++) {
             if (i > 1) defineBuilder.append(", ");
-            defineBuilder.append("A").append(i).append(" AS a_").append(i).append(" > 0");
+            defineBuilder.append("A").append(i).append(" AS a_").append(i).append(" >= ").append(DENSITY);
         }
 
         // 构建 PATTERN 部分
         for (int i = 1; i < Col; i++) {
             if (i > 1) patternBuilder.append(" AND ");
             patternBuilder.append("A").append(i)
-                    .append(" meets;met-by;overlaps;started-by;during;finished-by;equals ")
+                    .append(" meets;met-by;overlapped-by;overlaps;started-by;starts;during;contains;finishes;finished-by;equals ")
+//                    .append(" meets;met-by;overlaps;started-by;during;finished-by;equals ")
                     .append("A").append(i + 1);
         }
 
@@ -49,29 +50,29 @@ public class Main {
         return new Schema("CSV",  attriList);
     }
 
-    public static long buildRunner(int col, long row, String basePath, WindowType windowType) {
+    public static long buildRunner(int col, long row, String basePath, WindowType windowType,int DENSITY) {
         Schema schema = Main.buildSchema(col);
-        String query = Main.buildSimpleJoinQuery(col); // Assuming buildQuery is used here
+        String query = Main.buildSimpleJoinQuery(col,  DENSITY); // Assuming buildQuery is used here
 
         Engine engine = new Engine(schema, query, windowType);
         StringBuilder dataPath = new StringBuilder();
-        dataPath.append(basePath).append("col").append(col).append("_row").append(row).append(".csv");
+        dataPath.append(basePath).append("col").append(col).append("_row100000000").append(".csv");
 
         // Initialize FileDataSource and process data with the Engine
         try (DataSource dataSource = new FileDataSource(dataPath.toString())) {
             String line;
-            long cnt = 1;
+            long cnt = 0;
             long startTime = System.currentTimeMillis(); // Start timing
-            while ((line = dataSource.readNext()) != null) {
+            while ((line = dataSource.readNext()) != null && cnt<row) {
                 engine.apply("", line); // Process each line of data
                 cnt++;
-                if (cnt % 100000 == 0) {
-                    System.out.println(cnt + ", ");
-                }
+//                if (cnt % 100000 == 0) {
+//                    System.out.println(cnt + ", ");
+//                }
             }
             long endTime = System.currentTimeMillis();
 
-            System.out.println("\nTotal Lines Processed: " + (cnt-1));
+            System.out.println("\nTotal Lines Processed: " + (cnt));
             System.out.println("Processing time: " + (endTime - startTime) + " ms");
             engine.printResultCNT();
             engine.printAccumulatedTimes();
@@ -84,20 +85,22 @@ public class Main {
     }
 
 
+
     public static void main(String[] args) {
 
+        int  density=10;
         List<Integer> colList = new ArrayList<>(Arrays.asList(4 ));
 
 //        List<Integer> colList = new ArrayList<>(Arrays.asList(4, 6));
 //        List<Integer> colList = new ArrayList<>(Arrays.asList( 4));
 
-        List<Long> rowList = new ArrayList<>(Arrays.asList(1000_000L ));
+        List<Long> rowList = new ArrayList<>(Arrays.asList(100_000L,1000_000L,10_000_000L,100_000_000L ));
 //        List<Long> rowList = new ArrayList<>(Arrays.asList(100_000L, 1000_000L, 10_000_000L, 100_000_000L));
 
         WindowType windowType=WindowType.TIME_WINDOW;
 //        String basePath = "/Users/czq/Code/TPstream0/TPStream_DAPD/jepc-v2/";
 
-        String basePath = "/home/uzi/Code/TPS/jepc-v2/";
+        String basePath = "/home/uzi/Code/TPS/jepc-v2/data/";
         Map<Integer, Map<Long, Long>> col_row_proceTimeMap = new HashMap<>();
 
         for (int col : colList) {
@@ -106,7 +109,7 @@ public class Main {
 
             for (long row : rowList) {
                 System.out.println("===============  COL "+col+" ROW "+row+" ===============");
-                Long processedTime = buildRunner(col, row, basePath, windowType);
+                Long processedTime = buildRunner(col, row, basePath, windowType,  density);
                 col_row_proceTimeMap.get(col).put(row, processedTime);
                 System.out.println("(0.0)");
                 System.out.println(col_row_proceTimeMap);
@@ -116,8 +119,8 @@ public class Main {
     }
 }
 
-
-//java -Xms20g -Xmx20g  -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="test_service_jvmDump.hprof"  -cp "target/classes:lib/*" org.example.Main
+// java  -Xms12g -Xmx12g  -cp "target/classes:lib/*" org.example.Main
+//java  -Xms12g -Xmx12g   -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath="test_service_jvmDump.hprof"  -cp "target/classes:lib/*" org.example.Main
 // row = 1000000, window=100000
 //{16=25558, 18=28040, 4=5680, 20=32368, 6=8413, 22=35275, 8=10719, 10=15273, 12=17323, 14=22095}
 
